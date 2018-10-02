464a4bdc79e91e217e64c2458d1d23b56f455980
ulfara98 <43521662+ulfara98@users.noreply.github.com>
2018-10-02 16:29:34 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,26 @@
+<form action="" method="POST">
+	<center>
+		<body bgcolor="#b2ffef">
+		<br><br><br><br><br><br><br><br><br><br>
+		<table border="1" bgcolor="pink">
+			<tr bgcolor="yellow">
+				<td>Komentar 	: </td>
+				<td><input type="textbox" name="komentar"></td>
+			</tr>
+			<tr>
+				<td><input type="Submit" name="submit" value="Hitung Kata"></td>
+			</tr>
+		</table>
+</body>
+</center>
+</form>
+
 <?php
+	if(isset($_POST['submit'])){
+		$komentar = $_POST['komentar'];
+		$hasil = str_word_count($komentar);
+
+		echo "<center>Jumlah kata pada komentar adalah : $hasil</center>";
+	}
 
 ?>
